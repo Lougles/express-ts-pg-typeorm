4fa465b95dfe2bc963173602bfbfb7f68d7ccec0
vovacelidze <louglescom@gmail.com>
2023-06-19 11:15:26 +0300

    added basic configuration to start development

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+const logger = require('morgan')
+const cors = require('cors');
+
+const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
